32b30e2dad5e22d920455ac859ce11dbe9819cfc
Paul DiLoreto <soodohh@pm.me>
2024-02-17 21:02:27 +0000

    feat: added husky & commitlint

---
+++ commitlint.config.ts
@@ -0,0 +1,14 @@
+import { RuleConfigSeverity } from "@commitlint/types";
+import type { UserConfig } from "@commitlint/types";
+
+const Configuration: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [
+      RuleConfigSeverity.Error,
+      "never",
+    ],
+  },
+};
+
+module.exports = Configuration;
