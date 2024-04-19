350ce0458c8ccc0348cdce765b5687fec4a53a1a
Paul DiLoreto <soodohh@pm.me>
2024-04-19 12:30:12 -0700

    feat: add home assistant

--- update-portainer.sh
+++ update-portainer.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 docker stop portainer
+
 # Check if the container with the name "portainer" exists
 if [ "$(docker ps -aq -f name=portainer)" ]; then
     echo "Removing container named 'portainer'..."
     docker rm portainer
 fi
+
 # Check if the volume named "portainer_data" exists
 if ! docker volume inspect portainer_data &> /dev/null; then
     echo "Creating volume named 'portainer_data'..."
@@ -13,12 +15,21 @@ if ! docker volume inspect portainer_data &> /dev/null; then
 else
     echo "Volume named 'portainer_data' already exists."
 fi
+
 # Check if the network named "proxy" exists
 if ! docker network inspect proxy &> /dev/null; then
     echo "Creating network named 'proxy'..."
     docker network create proxy
 else
     echo "Network named 'proxy' already exists."
+fi
+
+# Check if the network named "proxy" exists
+if ! docker network inspect arr_network &> /dev/null; then
+    echo "Creating network named 'arr_network'..."
+    docker network create arr_network
+else
+    echo "Network named 'arr_network' already exists."
 fi
 
 docker run -d \
