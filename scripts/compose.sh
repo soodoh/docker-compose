b5a65bc1e4fcfdbff01770b099e1dcf6d3598675
Paul DiLoreto <paul.diloreto@gmail.com>
2024-10-07 23:23:27 -0700

    feat: combine docker-compose command into script

---
+++ scripts/compose.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker compose --env-file .env \
+  -f ./compose/apps.yml \
+  -f ./compose/hass.yml \
+  -f ./compose/nextcloud.yml \
+  -f ./compose/downloaders.yml \
+  -f ./compose/wger.yml \
+  -f ./compose/auth.yml \
+  -f ./compose/infra.yml \
+  "$@"
