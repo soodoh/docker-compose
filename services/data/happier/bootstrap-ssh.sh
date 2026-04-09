b288e9684364fbecd2d2e3c6ed6d5324de4a5ff3
Paul DiLoreto <soodohh@pm.me>
2026-04-08 23:47:03 -0700

    fix: pin happier devbox tool installs

--- services/data/happier/bootstrap-ssh.sh
+++ services/data/happier/bootstrap-ssh.sh
@@ -8,32 +8,27 @@ KNOWN_HOSTS_FALLBACK="/usr/local/share/happier/github_known_hosts"
 
 install -d -m 700 -o "${DEVBOX_USER}" -g "${DEVBOX_USER}" "${SSH_DIR}"
 
-copy_secret() {
+install_if_present() {
   local source_path="$1"
   local destination_path="$2"
   local mode="$3"
 
   if [[ -s "${source_path}" ]]; then
     install -m "${mode}" -o "${DEVBOX_USER}" -g "${DEVBOX_USER}" "${source_path}" "${destination_path}"
-  elif [[ -e "${destination_path}" ]]; then
-    return 0
-  else
-    install -m "${mode}" -o "${DEVBOX_USER}" -g "${DEVBOX_USER}" /dev/null "${destination_path}"
   fi
 }
 
-copy_secret /run/secrets/github_ssh_key "${SSH_DIR}/id_ed25519" 600
-copy_secret /run/secrets/github_ssh_key.pub "${SSH_DIR}/id_ed25519.pub" 644
+install_if_present /run/secrets/github_ssh_key "${SSH_DIR}/id_ed25519" 600
+install_if_present /run/secrets/github_ssh_key.pub "${SSH_DIR}/id_ed25519.pub" 644
 
 if [[ -s /run/secrets/known_hosts ]]; then
   install -m 644 -o "${DEVBOX_USER}" -g "${DEVBOX_USER}" /run/secrets/known_hosts "${SSH_DIR}/known_hosts"
-elif [[ -s "${KNOWN_HOSTS_FALLBACK}" && ! -s "${SSH_DIR}/known_hosts" ]]; then
+elif [[ ! -s "${SSH_DIR}/known_hosts" && -s "${KNOWN_HOSTS_FALLBACK}" ]]; then
   install -m 644 -o "${DEVBOX_USER}" -g "${DEVBOX_USER}" "${KNOWN_HOSTS_FALLBACK}" "${SSH_DIR}/known_hosts"
-elif [[ ! -e "${SSH_DIR}/known_hosts" ]]; then
-  install -m 644 -o "${DEVBOX_USER}" -g "${DEVBOX_USER}" /dev/null "${SSH_DIR}/known_hosts"
 fi
 
-cat > "${SSH_DIR}/config" <<'EOF'
+if [[ -s "${SSH_DIR}/id_ed25519" ]]; then
+  cat > "${SSH_DIR}/config" <<'EOF'
 Host github.com
   HostName github.com
   User git
@@ -41,6 +36,14 @@ Host github.com
   IdentitiesOnly yes
   StrictHostKeyChecking yes
 EOF
+else
+  cat > "${SSH_DIR}/config" <<'EOF'
+Host github.com
+  HostName github.com
+  User git
+  StrictHostKeyChecking yes
+EOF
+fi
 
 chown "${DEVBOX_USER}:${DEVBOX_USER}" "${SSH_DIR}/config"
 chmod 600 "${SSH_DIR}/config"
